89baddb5e26d6c9f3a9d9490cb1fbdab40373bc4
Ghassan Emran <ghassan.98@yahoo.com>
2025-10-26 13:00:16 +0300

    edit vite confige.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/My-Portfolio/", // important: set the base path here
+  base: "/", // ✅ root path for Vercel
 });
